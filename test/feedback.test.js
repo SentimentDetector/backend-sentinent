52022b8d5ce279ad769ac7e25da7ff8360ef594f
Steven1805 <hei.steven.2@gmail.com>
2024-02-18 15:58:11 +0300

    chore: fix code coverage

--- test/feedback.test.js
+++ test/feedback.test.js
@@ -1,7 +1,7 @@
 import getFeedback from '../modules/feedback.js';
 
 describe('getFeedback', () => {
-  test('should generate feedback based on provided reactions summary', async () => {
+  test('should give feedback from reactions summary', async () => {
     // Mock reactions summary
     const reactionsSummary = {
       totalLikes: 10,
@@ -17,10 +17,14 @@ describe('getFeedback', () => {
 
     // Assert the output
     expect(result).toEqual({
-      totalScore: 24, // (10 * 1) + (5 * 2) + (2 * 1) + (3 * 1) + (1 * -1) + (0 * -2)
-      feedback: 'Your sentiment score is influenced by the types and quantity of reactions on your posts. Your positive interactions outweigh the negative ones, your posts are radiating positivity! Your audience loves engaging with your content.'
-    });
-  });
+      totalScore: 24,
+      feedback: 'Your sentiment score is influenced by the types '+
+      'and quantity of reactions on your posts. ' +
+      'Your positive interactions outweigh the negative'+
+      ' ones, your posts are radiating positivity! ' +
+      'Your audience loves engaging with your content.'
+});
+});
 
   test('should generate neutral feedback when total score is 0', async () => {
     const reactionsSummary = {
@@ -36,8 +40,12 @@ describe('getFeedback', () => {
   
     expect(result).toEqual({
       totalScore: 0,
-      feedback: 'Your sentiment score is influenced by the types and quantity of reactions on your posts. You have an equal balance of positive and negative interactions, your posts are maintaining a neutral sentiment. Spice things up with diverse content to keep your audience engaged.'
-    });
+      feedback: 'Your sentiment score is influenced by the types and'+
+      ' quantity of reactions on your posts. ' +
+      'You have an equal balance of positive and negative'+
+      ' interactions, your posts are maintaining a neutral sentiment. ' +
+      'Spice things up with diverse content to keep your audience engaged.'
+  });
   });
   
   test('should generate neutral feedback when total score is 0', async () => {
@@ -54,7 +62,11 @@ describe('getFeedback', () => {
   
     expect(result).toEqual({
       totalScore: -3,
-      feedback: 'Your sentiment score is influenced by the types and quantity of reactions on your posts. Negative interactions have a higher impact, some of your posts may be causing concern. Consider addressing any issues and fostering a more positive community.'
-    });
+      feedback: 'Your sentiment score is influenced by the'+
+      ' types and quantity of reactions on your posts. ' +
+      'Negative interactions have a higher impact, some'+
+      ' of your posts may be causing concern. ' +
+      'Consider addressing any issues and fostering a more positive community.'
+  });
   });
 });
